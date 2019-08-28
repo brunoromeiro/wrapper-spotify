1503410047589636af3007370623bf8765321ca2
Bruno Romeiro <bruno.romeiro@gmail.com>
2019-08-28 17:52:31 -0300

    feat(globals): configura variaveis globais

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const API_URL = 'api.spotify.com/v1';
+
+export const token = 'BQC-WY_y3fLiheNTSjerGnHWFNqo5w8DcPAlqWFOwnChMt7QItpfuPPu2EFyDovG9yat0xB3z_hhDLSO3d0rURwDp1LZGJvJTXoTi9ztsIEge17npaP76b1dPIQg8K-GnCFr9bBRtdYBkHL50Fs'
