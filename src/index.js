c4d22038df3fd505d7041ffe8b9bc86450c853f0
Bruno Romeiro <bruno.romeiro@gmail.com>
2019-08-28 17:52:04 -0300

    feat(index.js): adiciona e condigura index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {
+  search,
+  searchAlbuns,
+  searchArtists,
+  searchTracks,
+  searchPlaylists
+} from './search';
+
+import {
+  getAlbuns,
+  getAlbum,
+  getAlbumTracks
+} from './album';
+
+module.exports = {
+  search,
+  searchAlbuns,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+  getAlbuns,
+  getAlbum,
+  getAlbumTracks
+}
